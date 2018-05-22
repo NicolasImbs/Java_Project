442c2d841cdc751af2245b8827130301a77867a9
Yonkt <yonkt77@gmail.com>
2018-05-22 22:34:46 +0200

    ajout distance Edge.java

--- src/Edge.java
+++ src/Edge.java
@@ -1,14 +1,27 @@
 
 public class Edge<V> {
 	private final V source, destination;
+	private int dist;
+
 	
-	public Edge(V s, V d) {
+	public Edge(V s, V d, int length) {
 		this.source = s;
 		this.destination = d;
+		this.dist = dist;
 	}
 	
 	public V from() {return source;}
 	public V to() {return destination;}
+	public int getDistance() {return dist;}
 	
-	// bonjour
+	
+//	public int getNeighbourIndex(int nodeIndex) {
+//	    if (this.source == nodeIndex) {
+//	      return this.destination;
+//	    } else {
+//	      return this.source;
+//	   }
+//	  }
+//	
+	
 }
